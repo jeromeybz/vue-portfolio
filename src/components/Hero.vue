42cc100ebbde86b9b78004b7ea68b6142ddae29d
jeromesed <marcjeromecolobong@gmail.com>
2026-03-07 10:19:17 +0800

    resume update

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -15,7 +15,7 @@
         A Computer Science student at Mariano Marcos State University. I do software development and data analytics for fun).
       </p>
       <a
-        href="/`Colobong_Resume`.pdf"
+        href="/`Colobong_Resume``.pdf"
         download
         class=" inline-block mt-5 pdx-7 py-2 rounded hover:opacity-100 transition text-2xl "
         style="background-color: #42b883; color: #FBEFEF; font-weight: bold;"
